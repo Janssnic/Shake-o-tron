a3d347179c077a32d9213fcd8e931919d88d6354
Janssnic <janssnic@arcada.fi>
2024-09-29 23:11:44 +0300

    fix?

--- ShakeOTron/Scripts/ApiDrink.js
+++ ShakeOTron/Scripts/ApiDrink.js
@@ -1,50 +1,37 @@
-//hello
 let drinksOfType = [];
 
-export function apiResponse(alcType){
-    alcType = "Vodka"
-    //add someway to change this varible to accepted alchol base for drink
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcType
+function apiResponse(alcType) {
+  alcType = "Vodka"
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcType
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data =>{
-            const drinks = data.drinks;
-            if (drinks && drinks.length > 0) {
-                drinks.forEach(element => {
-                    const id = element.idDrink
-                    const name = element.strDrink
-                    const pic = element.strDrinkThumb
-                    //console.log(element)
-
-                    matrixGen(id, name, pic)
-                });
-            }
-        }
-    )
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const drinks = data.drinks;
+      if (drinks && drinks.length > 0) {
+        drinks.forEach(element => {
+          const id = element.idDrink
+          const name = element.strDrink
+          const pic = element.strDrinkThumb
+          matrixGen(id, name, pic)
+        });
+      }
+      console.log(exportDrinks());
+    })
 }
 
-export function matrixGen(id, name, pic){
-    const oneDrinkArray = [id, name, pic];
-    drinksOfType.push(oneDrinkArray);
+function matrixGen(id, name, pic) {
+  const oneDrinkArray = [id, name, pic];
+  drinksOfType.push(oneDrinkArray);
 }
 
-export function showMatrix(){
-    apiResponse()
-    setTimeout(() => {
-        //console.log(drinksOfType)
-        exportDrinks()
-    }, 2000)
+function exportDrinks() {
+  const formattedDrinks = drinksOfType.map(drink => ({
+    id: drink[0],
+    name: drink[1],
+    pic: drink[2]
+  }));
+  return formattedDrinks;
 }
 
-export function exportDrinks() {
-    const formattedDrinks = drinksOfType.map(drink => ({
-        name: drink[1],
-        pic: drink[2]
-    }));
-    //console.log(formattedDrinks);
-    return formattedDrinks;
-}
-
-export { drinksOfType as drinks };
-//take pictures and name from this and show on page when youve chosen alchol base
+export { apiResponse, exportDrinks };
---
+++ ShakeOTron/shakeotron-app/src/components/DrinksApi.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+      <li v-for="drink in drinks" :key="drink.id">
+        {{ drink.name }} ({{ drink.id }})
+        <img :src="drink.pic" alt="Drink image">
+      </li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        drinks: exportDrinks()
+      }
+    }
+  }
+  </script>
