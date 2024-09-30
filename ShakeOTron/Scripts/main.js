a0d8789f3bbf72a7d84a17bde1281f0a3bdf156f
morris <bjornimo@gmail.com>
2024-09-30 13:05:34 +0300

    PrimeVue Installerat

--- ShakeOTron/Scripts/main.js
+++ ShakeOTron/Scripts/main.js
@@ -1,1 +1,18 @@
 //hello
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from "primevue/button"
+
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.component('Button', Button);
+app.mount("#app")
