f40aab03ddbc611cad2eca0209712abf8e6b4a55
Janssnic <janssnic@arcada.fi>
2024-10-01 11:26:26 +0300

    drinkApi

--- ShakeOTron/shakeotron-app/src/components/DrinksApi.vue
+++ ShakeOTron/shakeotron-app/src/components/DrinksApi.vue
@@ -1,26 +1,45 @@
 <template>
+    <div>
+      <input type="text" v-model="searchQuery" @keyup.enter="searchDrinks">
+    </div>
     <ul>
       <li v-for="drink in drinks" :key="drink.id">
-        {{ drink.name }} ({{ drink.id }})
-        <img :src="drink.pic" alt="Drink image">
+        {{ drink.strDrink }}
+        <img :src="drink.strDrinkThumb" alt="Drink image" @click="getThisDrink(drink.idDrink)">
       </li>
     </ul>
 </template>
   
-  <script>
-  import { apiResponse, exportDrinks } from '../../../Scripts/ApiDrink';
-  
-  
-  export default {
-    data() {
-      return {
-        drinks: []
+<script>
+export default {
+  data() {
+    return {
+      drinks: [],
+      searchQuery: '',
+    };
+  },
+
+
+  methods: {
+    async getData(alcType){
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcType
+      try {
+        let response = await fetch(url)
+        let data = await response.json()
+        this.drinks = data.drinks
+        //console.log(this.drinks)
       }
-    },
-    mounted() {
-      apiResponse('Vodka').then(() => {
-        this.drinks = exportDrinks();
-      });
-    }
+      catch (error) {
+          console.log(error);
+        }
+      },
+      searchDrinks() {
+        this.getData(this.searchQuery)
+      },
+      getThisDrink(idDrink) {
+        console.log(idDrink)
+      }
   }
-  </script>
+}
+
+</script>
