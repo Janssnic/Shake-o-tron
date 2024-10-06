f5cf9f8fc968d683ce3678688b70d168a26ca592
GerkJa <gerkmanj@arcada.fi>
2024-10-06 23:33:59 +0300

    Login test

---
+++ ShakeOTron/shakeotron-app/src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>{{ name }}</h1>
+    <div>
+   
+        
+   
+   </div>
+   
+   </template>
+   
+   <script>
+   
+   export default {
+       name: 'Login',
+       props: ['name'],
+       
+     }
+   </script>
+   
+   <!-- Add "scoped" attribute to limit CSS to this component only -->
+   <style scoped>
+   div {
+     padding: 0px;
+     margin: 0px;
+     display: flex;
+     justify-content: center;
+     flex-direction: row;
+     position: sticky;
+     bottom: 0;
+     overflow: hidden;
+     background-color: #333;
+   }
+   h3 {
+     margin: 40px 0 0;
+   }
+   ul {
+     list-style-type: none;
+     padding: 1%;
+   }
+   li {
+     display: inline-block;
+     margin: 0 10px;
+   }
+   a {
+     color: #42b983;
+   }
+   </style>
