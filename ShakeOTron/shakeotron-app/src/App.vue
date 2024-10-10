ad99e27911c475a03a9a3e7c2f50ed2bd0b0a79d
Jens <klingaje@arcada.fi>
2024-10-10 10:41:37 +0300

    liked drinks

--- ShakeOTron/shakeotron-app/src/App.vue
+++ ShakeOTron/shakeotron-app/src/App.vue
@@ -2,6 +2,7 @@
   <NavigationComponent/>
   <LoginComponent/>
   <DrinksApi @likedDrink="addLikedDrink" @testedDrink="addTestedDrink"></DrinksApi>
+  <LikedDrinks ref="likedDrinksComponent"/>
   
 </template>
 
@@ -11,6 +12,7 @@
 import DrinksApi from './components/DrinksApi.vue';
 import LoginComponent from './components/LoginComponent.vue';
 import NavigationComponent from './components/NavigationComponent.vue';
+import LikedDrinks from './components/LikedDrinks.vue';
 
 
 export default {
@@ -18,53 +20,24 @@ export default {
   components: {
     LoginComponent,
     NavigationComponent,
-    DrinksApi
+    DrinksApi,
+    LikedDrinks
   },
   data() {
     return {
-      likedDrinks: [],
-      testedDrinks: []
+      
     }
   },
-  created() { //checkar om det finns liked drinks i localstorage och sparar dem i likeddrinks arrayn
-    const storedDrinks = localStorage.getItem("likedDrinks")
-    if (storedDrinks) {
-      this.likedDrinks = JSON.parse(storedDrinks)
-    }
-    else {
-      this.likedDrinks = []
-    }
-    console.log("stored drinks", this.likedDrinks)
-
-    const storedTested = localStorage.getItem("testedDrinks")
-    if (storedTested) {
-      this.testedDrinks = JSON.parse(storedTested)
-    }
-  },
+  
   methods: {
-    addLikedDrink(drink) { //checkar om liked drink redan är liked och lägger den till arrayn av likedDrinks samt sparar i local storage
-      let alreadyLiked = false
-      for (let i = 0; i < this.likedDrinks.length; i++)
-        if (this.likedDrinks[i].idDrink === drink.idDrink) {
-          alreadyLiked = true
-          break;
-        }
-
-      if (!alreadyLiked) {
-        this.likedDrinks.push(drink)
-        localStorage.setItem('likedDrinks', JSON.stringify(this.likedDrinks))
-        console.log("added", drink, " to liked drinks")
-      }
+    addLikedDrink(drink) {
+      this.$refs.likedDrinksComponent.addLikedDrink(drink);
     },
-    addTestedDrink(drink) { //en array med 5 senaste testade drinks
-      if (this.testedDrinks.length >= 5) {
-        this.testedDrinks.shift() //deletar första item
-      }
-      this.testedDrinks.push(drink) //lägger till ny item
-      localStorage.setItem('testedDrinks', JSON.stringify(this.testedDrinks))
-      console.log("added", drink, " to tested drinks")
-    }
-  }
+    addTestedDrink(drink) {
+      this.$refs.likedDrinksComponent.addTestedDrink(drink);
+  },
+  
+}
 }
 </script>
 
