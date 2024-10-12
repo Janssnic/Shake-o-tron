16d43ef7fcc24efc6c905899b159ecf276e298f9
GerkJa <gerkmanj@arcada.fi>
2024-10-13 00:54:06 +0300

    started change username fature

--- ShakeOTron/shakeotron-app/src/components/AccountComponent.vue
+++ ShakeOTron/shakeotron-app/src/components/AccountComponent.vue
@@ -1,6 +1,18 @@
 <template>
     <div id="account-container">
-        <div id="left-container"></div>
+        <div id="left-container">
+            <div>
+            <p>Username: {{ username }}</p>
+            <input type="text" placeholder="change username">
+            </div>
+            
+            <div>
+            <p>Email: {{ email }}</p>
+            </div>
+            
+            <p>Your favourite cocktail is: </p>
+            <p>{{ favCocktail }}</p>
+        </div>
         <div id="right-container">
             <button @click="logout">Logout</button>
             <SettingsPage/>
@@ -14,6 +26,22 @@
     export default {
         components: {
             SettingsPage
+        },
+        data(){
+            return {
+                username: '',
+                password: '',
+                email: '',
+                favCocktail: 'home-made Apple Wine',
+            }
+        },
+        mounted() {
+            if (localStorage.getItem('user') !== null) {
+                this.username = JSON.parse(localStorage.getItem('user'))[0]
+                this.email = JSON.parse(localStorage.getItem('user'))[1]
+                this.password = JSON.parse(localStorage.getItem('user'))[2]
+                this.favCocktail = JSON.parse(localStorage.getItem('user'))[3]
+            }
         },
         methods: {
             logout() {
@@ -29,6 +57,14 @@ li {
   font-size: x-large;
   color: #3b3b3b;
   cursor: pointer;
+}
+#account-container{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+#left-container{
+    padding: 15px;
 }
 button {
     margin-top: 20px;
